877e135a46e5cc81b25ae482a496dd9173ea4db1
FelixGitHu <132509492+FelixGitHu@users.noreply.github.com>
2023-08-31 20:07:40 -0300

    Action y Reducer
    Agregar las nuevas actiones y el reducer de antes

--- client/src/redux/Actions/action.ts
+++ client/src/redux/Actions/action.ts
@@ -1,4 +1,24 @@
 import axios from "axios";
+import { Dispatch } from 'redux';
+
+export type Action = {
+    type: string;
+    payload?: any;
+  };
+export function searchArtist(name:string) {
+  return async function search(dispatch: Dispatch<Action>) {
+    try {
+      const response = await axios.get(`http://localhost/getPosts?name=${name}`);
+        dispatch({
+          type: 'SEARCH_ARTIST',
+          payload: response.data,
+        });
+    } catch (error) {
+      console.log(error);
+      alert('No se encontro el artista con ese nombre');
+    }
+  }
+}
 
 const llamadoAlBackend = async( creation: {
     title: string;
@@ -35,4 +55,20 @@ export const postCreation = (creation: {
         type: 'CREATE_POST',
         payload: dbCreation
     }
+}
+
+export function prev() {
+  return {
+    type: 'PREV',
+  };
+}
+export function next() {
+  return {
+    type: 'NEXT',
+  };
+}
+export function resetPage() {
+  return {
+    type: 'RESET_PAGE',
+  };
 }
--- client/src/redux/Reducer/reducer.ts
+++ client/src/redux/Reducer/reducer.ts
@@ -1,6 +1,16 @@
-
+interface ArtGalleryItem {
+    id: string;
+    imageURL: string;
+    type: string;
+    name: string;
+    artistName: string;
+    cost: string;
+  }
+  
 export type InitialState<T> = {
     allPosts: T,
+    numPage: number,
+    artGallery: ArtGalleryItem[],
 }
 
 type Action<T> = {
@@ -10,16 +20,44 @@ type Action<T> = {
 
 const initialState: InitialState<any> = {
     allPosts: [],
+    numPage:1,
+    artGallery:[],
 }
 
 
 export default function rootReducer(state = initialState, action:Action<any>){
     switch(action.type){
-      case 'CREATE_POST':
+        case 'CREATE_POST':
               return {
                 ...state,
                 allPosts: [action.payload, ...state.allPosts],
               };
+        case 'SEARCH_ARTIST':
+                let artist: Array<any> = [];
+                if (action.payload.length === 0) {
+                  artist = state.artGallery;
+                } else {
+                  artist = action.payload.map((ele: string) => ele);
+                }
+                return {
+                    ...state,
+                    searchArtist: artist,
+                  };
+        case 'RESET_PAGE':
+                return {
+                    ...state,
+                    numPage: 1,
+                };
+        case 'PREV':
+                return {
+                    ...state,
+                    numPage: state.numPage - 1,
+                };
+        case 'NEXT':
+                return {
+                    ...state,
+                    numPage: state.numPage + 1,
+                };
           default:
               return {...state};
     }
