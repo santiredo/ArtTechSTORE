9b3a4198372c079124cfdb5dfdeac72813fb9e41
unknown <bmchucop@gmail.com>
2023-08-31 08:33:26 -0500

    Register and Detail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
-
-import './App.css'
-
-
+import { useLocation, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Register from "./components/Register/Register";
+import Detail from './components/Detail/Detail';
 
 function App() {
+  
+    const location = useLocation()
 
-    return (
-        <></>
-    )
-
-
+    {location.pathname === '/' ? undefined : <Detail />}
+      return (
+        <>
+        <Routes>
+          <Route path='/' element={<Register/>}/>
+          <Route path='/detail' element={<Detail/>}/>
+        </Routes>
+        </>
+      )
+    
 }
 
 export default App
