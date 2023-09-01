6008a56aac1bb9c5ea7abfdc54965cf95a95ae10
santiRedondo <santi16redo@gmail.com>
2023-09-01 00:53:34 -0300

    Completando el Reducer

--- client/src/redux/Reducer/reducer.ts
+++ client/src/redux/Reducer/reducer.ts
@@ -1,26 +1,68 @@
-
-export type InitialState<T> = {
-    allPosts: T,
+interface ArtGalleryItem {
+  id: string;
+  imageURL: string;
+  type: string;
+  name: string;
+  artistName: string;
+  cost: string;
 }
 
-type Action<T> = {
-    type:string;
-    payload: T
+export type InitialState = {
+  allPosts: ArtGalleryItem[],
+  numPage: number,
+  artGallery: ArtGalleryItem[],
+};
+
+type Action<T> ={
+  type:string;
+  payload:T;
 }
 
-const initialState: InitialState<any> = {
-    allPosts: [],
-}
-
+const initialState: InitialState = {
+  allPosts: [],
+  artGallery: [],
+  numPage: 1,
+};
 
 export default function rootReducer(state = initialState, action:Action<any>){
-    switch(action.type){
-      case 'CREATE_POST':
-              return {
-                ...state,
-                allPosts: [action.payload, ...state.allPosts],
-              };
-          default:
-              return {...state};
-    }
+  switch(action.type){
+    case 'SEARCH_ARTIST':
+      let artist: Array<any> = [];
+
+      if (action.payload.length === 0) {
+        artist = state.artGallery;
+      } else {
+        artist = action.payload.map((ele: string) => ele);
+      }
+
+      return {
+        ...state,
+        searchArtist: artist,
+      };
+    case 'RESET_PAGE':
+      return {
+        ...state,
+        numPage: 1,
+      };
+    case 'PREV':
+      return {
+        ...state,
+        numPage: state.numPage - 1,
+      };
+    case 'NEXT':
+      return {
+        ...state,
+        numPage: state.numPage + 1,
+      };
+    case 'CREATE_POST':
+      return {
+        ...state,
+        allPosts: [action.payload, ...state.allPosts]
+      }
+    default:
+      return {...state};
+      
+      
   }
+}
+
