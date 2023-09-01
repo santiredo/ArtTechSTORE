e6d274910e54b3c66bb9197a72dac74d61a28b11
santiRedondo <santi16redo@gmail.com>
2023-09-01 12:41:41 -0300

    Modificando la ruta de importacion de componentes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,17 @@
 
-import { useLocation, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Landing from './views/Landing/Landing'
-import Register from "./components/Register/Register";
 import Detail from './components/Detail/Detail';
-import Card from './Card/Card';
 import './App.css';
 
 export default function App() {
-  const location = useLocation();
 
   return (
     <>
       <Routes>
         <Route path='/' element={<Landing/>}/>
-          <Route path='/detail/:id' element={<Detail/>}/>
-        </Routes>
-      </>
+        <Route path='/detail/:id' element={<Detail/>}/>
+      </Routes>
+    </>
   )
 };
