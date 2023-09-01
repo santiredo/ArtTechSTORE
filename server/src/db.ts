a6fb50e970fee4c09620f6abc1340a346c610441
santiRedondo <santi16redo@gmail.com>
2023-09-01 01:12:11 -0300

    Creacion de la db

---
+++ server/src/db.ts
@@ -0,0 +1,47 @@
+import { Sequelize, ModelCtor } from 'sequelize-typescript';
+import 'dotenv/config';
+
+import fs from 'fs'
+import path from 'path'
+import { useActionData } from 'react-router-dom';
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: Number(DB_PORT), // Convertir a número, ya que normalmente está en formato de cadena
+    database: 'artists',
+    logging: false,
+    native: false,
+});
+
+const basename = path.basename(__filename)
+
+const modelDefiners: ((sequelize: Sequelize) => void)[] = [];;
+
+fs.readdirSync(path.join(__dirname, '/models'))
+    .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+    .forEach((file) => {
+        modelDefiners.push(require(path.join(__dirname, '/models', file)));
+});
+
+
+modelDefiners.forEach((model: any) => model.default(sequelize))
+let entries = Object.entries(sequelize.models)
+
+const capitalizedModels: { [key: string]: ModelCtor<any>} = {}
+entries.forEach(([modelName, model]) => {
+    const capitalizedModelName = modelName.charAt(0).toUpperCase() + modelName.slice(1)
+    capitalizedModels[capitalizedModelName] = model as ModelCtor<any>;
+})
+
+const { Product, User } = sequelize.models
+
+
+User.belongsToMany(Product, {through: 'user_product'})
+Product.belongsToMany(User, {through: 'user_product'})
+
+export { sequelize };
