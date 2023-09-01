64725552921e570e426d89706d1a82026ba66c9b
santiRedondo <santi16redo@gmail.com>
2023-09-01 11:09:00 -0300

    Creando archivo para las relaciones de las tablas

---
+++ server/src/relations.ts
@@ -0,0 +1,8 @@
+import { sequelize } from './db'; 
+
+
+const { Product, User } = sequelize.models
+
+
+User.belongsToMany(Product, {through: 'user_product'})
+Product.belongsToMany(User, {through: 'user_product'})
