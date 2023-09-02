6ab470e6f558d8231eab2a4d47f80abf3ef60d7b
Gaston Stinson <gastonstinson00@gmail.com>
2023-09-02 19:58:46 -0300

    Corrigiendo Conflictos

--- server/src/relations.ts
+++ server/src/relations.ts
@@ -1,13 +1,6 @@
 import { sequelize } from "./db";
 
-const { Product, User, Comment } = sequelize.models;
+const { Product, User } = sequelize.models;
 
-//Relacion n:n
 User.belongsToMany(Product, { through: "user_product" });
 Product.belongsToMany(User, { through: "user_product" });
-
-//Relacion 1:n
-User.hasMany(Comment);
-
-//Comment pertenece a un usuario
-Comment.belongsTo(User);
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,4 @@ import { postCommentsHandler } from "../handlers/postComment.handler";
 
 const router = Router();
 
-router.get("/comments/:productId/:userId", postCommentsHandler);
-
 export default router;
