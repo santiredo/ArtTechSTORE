ba69f179bf61f83a907670108354cb8e1b6ab30b
Gaston Stinson <gastonstinson00@gmail.com>
2023-09-02 19:14:35 -0300

    Subiendo Controllers postComment

--- server/src/controllers/postComment.ts
+++ server/src/controllers/postComment.ts
@@ -1,21 +1,26 @@
 import { Comment } from "../models/Comment";
 
-export async function postComment(productId: string, userId: string, message: string, rating: number) {
-    try{
-        const comment = await Comment.create({
-            message,
-            rating,
-            productId,
-            userId
-        })
+export async function postComment(
+  productId: string,
+  userId: string,
+  message: string,
+  rating: number
+) {
+  try {
+    const comment = await Comment.create({
+      message,
+      rating,
+      productId,
+      userId,
+    });
 
-        // El comentario se ha creado exitosamente
-        console.log("Comentario creado:", comment.toJSON());
-        
-        return comment;
-    } catch(error: any){
-        // Manejar cualquier error que pueda ocurrir durante la creación del comentario
-        console.error("Error al crear el comentario:", error);
-        throw error; // Puedes decidir si quieres propagar el error o manejarlo de otra manera.
-    }
+    // El comentario se ha creado exitosamente
+    console.log("Comentario creado:", comment.toJSON());
+
+    return comment;
+  } catch (error: any) {
+    // Manejar cualquier error que pueda ocurrir durante la creación del comentario
+    console.error("Error al crear el comentario:", error);
+    throw error; // Puedes decidir si quieres propagar el error o manejarlo de otra manera.
+  }
 }
