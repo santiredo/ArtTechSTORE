389eac877188141ec887be4a8f5fe8a78ccb416f
FelixGitHu <132509492+FelixGitHu@users.noreply.github.com>
2023-09-02 19:43:43 -0300

    Ruta Status

---
+++ server/src/routes/status.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { createStatusHandler, deleteStatusHandler, getStatusByIdHandler, getStatusesHandler, updateStatusHandler } from "@/handlers/statusHandlers";
+
+const statusRouter = express.Router();
+
+statusRouter.post("/",createStatusHandler);
+statusRouter.get("/:id",getStatusByIdHandler);
+statusRouter.get("/",getStatusesHandler);
+statusRouter.put("/:id",updateStatusHandler);
+statusRouter.delete("/:id",deleteStatusHandler);
+
+
+export default statusRouter;
