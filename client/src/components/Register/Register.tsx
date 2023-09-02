c7c776ec5d4e9f43982a888ee7519c1cc96dc946
unknown <bmchucop@gmail.com>
2023-09-01 20:27:43 -0500

    Ventanas emergentes Login y Register

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,51 +1,65 @@
-import googleIcon from "../../images/google.png";
-import faceIcon from "../../images/facebook.png";
-import React from "react";
+import googleIcon from "../../assets/googleIcon.png";
+import faceIcon from "../../assets/facebookIcon.png";
+import React, {useState} from "react";
 import style from "./register.module.css";
+import Modal from 'react-modal';
 
-const Register = () => {
+Modal.setAppElement('#root');
+
+const RegisterModal = ({ isOpen, onRequestClose, onRegister }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleRegister = () => {
+        // Handle registration logic here
+        // You can use registrationData to get user input
+        // Call onRegister when registration is successful
+        onRegister();
+        onRequestClose(); // Close the registration modal
+    };
+
     return(
-        <>
+        <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        contentLabel="Login Modal"
+        className="custom-modal"
+        >
         <div className={style.container}>
-            <div className={style.registerComponent}>
-            <h1>REGISTER</h1>
-            
-            <div
-            className={style.regisInputs}>
-                <input type="text" placeholder="Name"/>
-                <input type="text" placeholder="Username"/>
-                <input type="text" placeholder="Address"/>
-                <input type="text" placeholder="Email"/>
-                <input type="text" placeholder="Password"/>
-                <input type="text" placeholder="Repeat your password"/>
-                <select className={style.list}>
-                <option value="option" disabled selected hidden>
-                    Choose an option
-                </option>
+            <form onSubmit={handleRegister}>
+                <h1>REGISTER</h1>
+                <div className={style.regisInputs}>
+                    <input type="text" placeholder="Name"/>
+                    <input type="text" placeholder="Username"/>
+                    <input type="text" placeholder="Address"/>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
+                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
+                    <input type="text" placeholder="Repeat your password"/>
+                    <select className={style.list}>
+                    <option value="option" disabled selected hidden>
+                        Choose an option
+                    </option>
                     <option value="seller">Seller</option>
                     <option value="customer">Customer</option>
-                </select>
-                <hr />
-                <button>CREATE NOW</button>                
-            </div>
+                    </select>
+                    <hr />
+                    <button type="submit">REGISTER</button>                
+                </div>
 
-            <div className={style.fastLogin}>
-                <p>Or Register with: </p>
-
-                <button className={style.fastLoginButton}>
+                <div className={style.fastLogin}>
+                    <p>Or Register with: </p>
+                    <button className={style.fastLoginButton}>
                     <img src={googleIcon} alt="" />
                     GOOGLE</button>
-                <button className={style.fastLoginButton}>
+                    <button className={style.fastLoginButton}>
                     <img src={faceIcon} alt="" />
                     FACEBOOK</button>
-            </div>
-
+                </div>
+                
+            </form>
         </div>
-        </div>
-        </>
-        
-        
+        </Modal>
     );
 };
 
-export default Register;
+export default RegisterModal;
