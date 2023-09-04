90b2359349765827fde8cec9cf2cb3448de33740
FelixGitHu <132509492+FelixGitHu@users.noreply.github.com>
2023-09-04 16:13:05 -0300

    Controlador user by name

--- server/src/controllers/user/getUserByName.ts
+++ server/src/controllers/user/getUserByName.ts
@@ -1,10 +1,13 @@
 import { User } from "../../db";
 
-export async function getUserByName(userName: string) {
-    const user = await User.findByPk(userName);
-  
+export async function getUserByName(name: string) {
+  const user = await User.findAll();
+  const userFound = user.filter((username: any) => username.name.toLowerCase() === name.toLowerCase());
+    console.log("Este es el usuario",user);
+    console.log("Este es el otro usuario",userFound);
+    
     if (!user) {
-      throw new Error('Favorito no encontrado');
+      throw new Error('Usuario no encontrado');
     }
   
     return user;
