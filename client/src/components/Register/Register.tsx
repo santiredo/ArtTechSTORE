de9e9ef522859e4bb085a3119ef7599c2d10f2c8
santi redondo <105244201+santiredo@users.noreply.github.com>
2023-09-05 20:42:29 -0300

    Update Register.tsx

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,9 +1,9 @@
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import googleIcon from "../../assets/googleIcon.png";
 import facebookIcon from "../../assets/facebookIcon.png";
 import style from "./register.module.css";
-import validation from "./validation";
+import validation, { validateSubmit } from "./validation";
 
 
 export default function Register({onRegister} : {onRegister: () => void})  {
@@ -12,6 +12,7 @@ export default function Register({onRegister} : {onRegister: () => void})  {
         name:'',
         mail:'',
         password:'',
+        repeatedPassword:'',
         birthDate:'',
         address:''
     })
@@ -23,22 +24,67 @@ export default function Register({onRegister} : {onRegister: () => void})  {
     }
     
     const[errors, setErrors] = useState({
+        name:'',
         mail:'',
         password:'',
+        repeatedPassword:'',
+        birthDate:''
     });
+
+    useEffect(() => {
+        setErrors(validation(registerForm))
+    }, [registerForm])
 
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value
+
+        console.log(event.target.name)
+        if(event.target.name === 'birthDate'){
+            value = value.replace(/-/g, '')
+
+            
+            if(value.length > 4){
+                value = value.slice(0, 4)+'-'+value.slice(4)
+                console.log(value)
+            }
+            if(value.length > 7){
+                value = value.slice(0, 7)+'-'+value.slice(7)
+                console.log(value)
+            }
+        }
+
+        console.log(value)
+
         setRegisterForm({
             ...registerForm,
-            [event.target.name]: event.target.value
+            [event.target.name]: value
         })
-        setErrors(
-            validation({
-                ...errors,
-                [event.target.name]: event.target.value
+
+        console.log(registerForm)
+    }
+
+    const handleRegister = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+
+        if(validateSubmit(registerForm)){
+            setErrors(validation(registerForm))
+            alert('You must fill the inputs correctly')
+        } else{
+            // ACA VENDRIA EL DISPATCH
+
+            setRegisterForm({
+                name:'',
+                mail:'',
+                password:'',
+                repeatedPassword:'',
+                birthDate:'',
+                address:''
             })
-        );
+            alert('Registered successfully')
+        }
+
+        
     }
 
     return (
@@ -47,19 +93,24 @@ export default function Register({onRegister} : {onRegister: () => void})  {
             <form className={style.registerComponent}>
                 <h1>CREATE YOUR ACOUNT</h1>
                 <div className={style.registerInputs}>
-                    <input type="text" name='name' placeholder="Username" onChange={handleChange}/>
+                    {<span>{errors.name}</span>}
+                    <input type="text" name='name' value={registerForm.name} placeholder="Username" onChange={handleChange}/>
                 </div>
                 <div className={style.registerInputs}>
-                    <input type="text" name='mail' placeholder="example@gmail.com" onChange={handleChange}/>
+                    {<span>{errors.mail}</span>}
+                    <input type="text" name='mail' value={registerForm.mail} placeholder="example@gmail.com" onChange={handleChange}/>
                 </div>
                 <div className={style.registerInputs}>
-                    <input type="pasword" name='password' placeholder="password" onChange={handleChange}/>
+                    {<span>{errors.password}</span>}
+                    <input type="pasword" name='password' value={registerForm.password} placeholder="password" onChange={handleChange}/>
                 </div>
                 <div className={style.registerInputs}>
-                    <input type="text" name='repeatedPassword' placeholder="repeat your password" onChange={handleChange}/>
+                    {<span>{errors.repeatedPassword}</span>}
+                    <input type="text" name='repeatedPassword' value={registerForm.repeatedPassword} placeholder="repeat your password" onChange={handleChange}/>
                 </div>
                 <div className={style.registerInputs}>
-                    <input type="text" name='birthDate' placeholder="YYYY-MM-DD" onChange={handleChange}/>
+                    {<span>{errors.birthDate}</span>}
+                    <input type="text" name='birthDate' maxLength={10} value={registerForm.birthDate} placeholder="YYYY-MM-DD" onChange={handleChange}/>
                 </div>
                 <div className={style.registerCheckbox}>
                     <label>Are you an artist?</label>
@@ -68,11 +119,11 @@ export default function Register({onRegister} : {onRegister: () => void})  {
                 {
                     isArtist && (
                         <div className={style.registerInputs}>
-                            <input type="text" name='address' placeholder="Here goes your address" onChange={handleChange}/>
+                            <input type="text" name='address' value={registerForm.address} placeholder="Here goes your address" onChange={handleChange}/>
                         </div>
                     )
                 }
-                    <button className={style.registerButton}>REGISTER</button>
+                <button onClick={handleRegister} className={style.registerButton}>Register</button>
                 <div className={style.fastRegister}>
                     <p>- Or -</p>
                     <button className={style.fastRegisterButton}><img src={googleIcon} alt=""/> Continue with google</button>
@@ -82,5 +133,4 @@ export default function Register({onRegister} : {onRegister: () => void})  {
             <button onClick={onRegister} className={style.quitRegisterButton}></button>
         </div>
     );
-}
-
+};
