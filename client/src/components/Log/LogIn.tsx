6b1a90294033fd4919737aa88063505917c48734
santiRedondo <santi16redo@gmail.com>
2023-09-05 10:19:08 -0300

    Modificando la logica de Login

--- client/src/components/Log/LogIn.tsx
+++ client/src/components/Log/LogIn.tsx
@@ -1,77 +1,69 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import googleIcon from '../../assets/googleIcon.png'
 import facebookIcon from '../../assets/facebookIcon.png'
 import style from './login.module.css'
-import Modal from 'react-modal';
 
-Modal.setAppElement('#root'); // Set the app root element
 
-const LoginModal = ({ isOpen, onRequestClose, openRegistration }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+export default function Login({onRegister} : {onRegister: () => void})  {
 
-       
-    const handleLogin = () => {
-    // Handle login logic here, e.g., send a request to your authentication API
-    // Close the modal after successful login
-    onRequestClose();
-    };
+    const [loginHandler, setLoginHandler] = useState(false)
+    const [loginForm, setLoginForm] = useState({
+        username:'',
+        password:''
+    })
 
-    
+    const showLogin = () => {
+        loginHandler ? setLoginHandler(false) : setLoginHandler(true)
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+        setLoginForm({
+            ...loginForm,
+            [event.target.name]: event.target.value
+        })
+    }
+
+
+
     return (
-        <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-        contentLabel="Login Modal"
-        className="custom-modal"
-        >
-        <div className={style.loginComponent}>
-            <form onSubmit={handleLogin}>
-                <h1>DO YOU HAVE AN ACCOUNT?</h1>
-                <div className={style.loginInputs}>
-                    <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    placeholder="Username"
-                    autoComplete="off"
-                    onChange={(e) => setUsername(e.target.value)}
-                    />
+
+        <>
+            <button className={style.loginButtonLanding} onClick={showLogin}>Log In</button>
+            {loginHandler && (
+                <div className={style.loginPage}>
+                    <form className={style.loginComponent}>
+                        <h1>DO YOU HAVE AN ACCOUNT?</h1>
+                        <div className={style.loginInputs}>
+                            <input
+                                type="text"
+                                name="username"
+                                value={loginForm.username}
+                                placeholder="Username"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className={style.loginInputs}>
+                            <input
+                                type="password"
+                                name="password"
+                                value={loginForm.password}
+                                placeholder="Password"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <button className={style.loginButton}>Log In</button>
+                        <div className={style.fastLogin}>
+                            <p>- Or -</p>
+                            <button className={style.fastLoginButton}><img src={googleIcon} alt=""/> Continue with google</button>
+                            <button className={style.fastLoginButton}><img src={facebookIcon} alt=""/> Continue with facebook</button>
+                        </div>
+                        <h4 onClick={() => { showLogin(); onRegister(); }}>Register Now</h4>
+                    </form>
+                    <button onClick={showLogin} className={style.quitLoginButton}></button>
                 </div>
-                <br />
-                <div className={style.loginInputs}>
-                    <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    placeholder="Password"
-                    autoComplete="off"
-                    onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <br />
-                <button className={style.btn} type="submit">Log In</button>
-                <br />
-                <br />
-                <li>
-                <button 
-                onClick={openRegistration}
-                className={style.btn}
-                >
-                    Register Now
-                </button>
-                </li>
-                <br />
-                <div className={style.fastLogin}>
-                    <p>- Or -</p>
-                    <button className={style.fastLoginButton}><img src={googleIcon} alt=""/> Continue with google</button>
-                    <button className={style.fastLoginButton}><img src={facebookIcon} alt=""/> Continue with facebook</button>
-                </div>
-                
-            </form>
-        </div>
-        </Modal>
+            )}
+        </>        
     )
 }
 
-export default LoginModal;
