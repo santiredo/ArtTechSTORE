f6d9aaab77f7217a4dc7f4ade19643157f44a596
FelixGitHu <132509492+FelixGitHu@users.noreply.github.com>
2023-09-08 12:11:23 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Landing from './views/Landing/Landing'
 import './App.css';
 import Display from './components/Display/Display';
 import Detail from './views/Detail/Detail';
+import Adminnuevo from './components/Admin/Admin+';
 import Admin from './components/Admin/Admin';
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Route path='/detail/:id' element={<Detail/>}/>
         <Route path='/display' element={<Display/>}/>
         <Route path='/admin' element={<Admin/>}/>
+        <Route path='/adminnuevo' element={<Adminnuevo/>}/>
       </Routes>
     </>
   )
