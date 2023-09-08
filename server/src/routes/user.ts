2e5702ee382352bd507710191c4cfafbc6457023
FelixGitHu <132509492+FelixGitHu@users.noreply.github.com>
2023-09-08 12:11:04 -0300

    Back end User
    Le agregue una ruta para obtener todos los usuarios

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -5,11 +5,13 @@ import { getUserByIdHandler } from "../handlers/User/getUserById";
 import { getUserByNameHandler } from "../handlers/User/getUserByName";
 import { postUserHandler } from "../handlers/User/postUser";
 import { updateUserHandler } from "../handlers/User/updateUser";
+import { getAllUserHandler } from "../handlers/User/getAllUser";
 
 const userRouter = express.Router();
 
 userRouter.get("/name",getUserByNameHandler);
 userRouter.get("/:id",getUserByIdHandler);
+userRouter.get("/",getAllUserHandler);
 userRouter.post("/",postUserHandler);
 userRouter.delete("/:id",deleteUserHandler);
 userRouter.put("/:id",updateUserHandler);
