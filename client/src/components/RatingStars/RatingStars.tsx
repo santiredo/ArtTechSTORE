e789b9cfedd55d1e89045cba2781e6b888a52071
santiRedondo <santi16redo@gmail.com>
2023-09-09 17:00:40 -0300

    Ajustando estilos de RatingStars

--- client/src/components/RatingStars/RatingStars.tsx
+++ client/src/components/RatingStars/RatingStars.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import blackStar from '../../assets/blackStar.png'
-import yellowStar from '../../assets/yellowStar.png'
+import emptyStar from '../../assets/emptyStar.png'
+import fullyStar from '../../assets/fullyStar.png'
 import style from  './rating.module.css'
 
 export default function RatingStars(){
@@ -13,19 +13,17 @@ export default function RatingStars(){
     return (
         <div className={style.ratingStarsComponent}>
           {ratingValues.map((value) => (
-            <div>
                 <label>
                 <input type="radio" name="rating" value={value} onClick={() => setRating(value)} />
                 {
                     value <= (hover! || rating!)
                     ? (
-                        <img className={style.ratingStar} src={yellowStar} onMouseEnter={() => setHover(value)} onMouseLeave={() => setHover(null)} />
+                        <img className={style.ratingStar} src={fullyStar} onMouseEnter={() => setHover(value)} onMouseLeave={() => setHover(null)} />
                     ) : (
-                        <img className={style.ratingStar} src={blackStar} onMouseEnter={() => setHover(value)} onMouseLeave={() => setHover(null)} />
+                        <img className={style.ratingStar} src={emptyStar} onMouseEnter={() => setHover(value)} onMouseLeave={() => setHover(null)} />
                     )
                 }
                 </label>
-            </div>
             
           ))}
         </div>
