d958eeed97b15fa65e70fbc85ba53069be68a095
santiRedondo <santi16redo@gmail.com>
2023-09-11 16:38:38 -0300

    modificando el path de la ruta getByName, y eliminando getArtistCreations

--- server/src/controllers/Artist/getArtistCreations.ts
+++
@@ -1,8 +0,0 @@
-import { Artist, Product } from "../../db";
-
-// Controlador para obtener todas las creaciones de un artista
-export async function getArtistCreations(artistId: number) {
-  return Artist.findByPk(artistId, {
-    include: Product, // Incluye las creaciones relacionadas con el artista
-  });
-}
