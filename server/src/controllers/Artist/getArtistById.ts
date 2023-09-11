e753e1aa32fa409345955c1c558564c6d57d52d0
santiRedondo <santi16redo@gmail.com>
2023-09-11 16:38:03 -0300

    Aregando los productos de los artistas al llamado por id

--- server/src/controllers/Artist/getArtistById.ts
+++ server/src/controllers/Artist/getArtistById.ts
@@ -1,11 +1,19 @@
+import { Product } from "../../db";
 import { Artist } from "../../db";
 
 export async function getArtistById(artistId: number) {
+  
     const artist = await Artist.findByPk(artistId);
   
+    const creations = await Product.findAll({
+      where: {
+        ArtistId: artistId
+      }
+    })
+
     if (!artist) {
       throw new Error('Artist not found');
     }
   
-    return artist;
+    return {...artist.dataValues, products: [...creations]};
   }
