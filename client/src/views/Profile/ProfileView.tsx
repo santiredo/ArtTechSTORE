7db8d5f5789972855abdfe71abe0a6d192572236
unknown <bmchucop@gmail.com>
2023-09-12 16:33:22 -0500

    Perfil del artista Avance

--- client/src/views/Profile/ProfileView.tsx
+++ client/src/views/Profile/ProfileView.tsx
@@ -1,12 +1,13 @@
 import  { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { getArtistById } from '../../redux/action';
-import { InitialState } from "../../redux/reducer";
+import { ArtGalleryItem, InitialState } from "../../redux/reducer";
 import { useParams } from "react-router-dom";
 import RatingStars from "../../components/RatingStars/RatingStars";
 import profilePhoto from '../../assets/usuario.png'
-import ultimaCreacion from '../../assets/fotoLanding4.jpg'
+//import ultimaCreacion from '../../assets/fotoLanding4.jpg'
 import styles from './ProfileView.module.css'
+import { Artist } from '../../redux/reducer';
 
 
 
@@ -37,20 +38,34 @@ export default function ProfileView() {
 
     getArtistById(id, dispatch)
     
-  }, []);
+  }, [id, dispatch]);
 
+  const [items, setItems] = useState<Artist[]>([]);
+  const [items2, setItems2] = useState<ArtGalleryItem[]>([]);
+
+    const URL='http://localhost:3001'
+    useEffect(() => {
+        fetch(`${URL}/artist/${id}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setItems(data.products);
+          setItems2(data.products);
+        });
+        
+      }, [id]);
 
   return (
     <div className={styles.profilePage}>
       <div className={styles.profileContainer}>
         <div className={styles.photoDiv}>
-          <img className={styles.profilePhoto} src={profilePhoto} alt="" />
+          <img className={styles.profilePhoto} src={artist.image ? artist.image : profilePhoto} alt="" />
           <RatingStars/>
         </div>
         <div className={styles.profileInfo}>
           <div className={styles.profileInfoDiv}>
             <h1>{artist.name}</h1>
-            <h3>{artist.location}Argentina</h3>
+            <h3>{artist.location}</h3>
           </div>
           <hr />
           <div className={styles.profileInfoDiv}>
@@ -59,16 +74,42 @@ export default function ProfileView() {
           </div>
             <div className={soldOut ? styles.showSoldOut : styles.hideSoldOut}>
               <p>Aqui van las creaciones vendidas</p>
+              
             </div>
             <div className={onSale ? styles.showOnSale : styles.hideOnSale}>
               <p>Aqui van las creaciones en venta</p>
+              {items2.map((card:ArtGalleryItem)=>
+              <div className={styles.cardComponent}>
+                <img src={card.image} alt="" />
+                <div className={styles.cardInfo}>
+                  <div className={styles.infoDiv}>
+                    <h3>${card.price}</h3>
+                    <button>Buy</button>
+                  </div>
+                </div>
+              </div>
+              
+              )}
             </div>
           <div className={styles.lastCreation}>
             <h2>Last post:</h2>
-            <img src={ultimaCreacion} alt="" />
+            {items.slice(-1).map((card:Artist) =>
+            (<div className={styles.cardComponent}>
+              <img key={card.id} src={card.image} alt="" />
+              <div className={styles.cardInfo}>
+                <div className={styles.infoDiv}>
+                  {items2.slice(-1).map((card:ArtGalleryItem)=>(
+                  <h3>${card.price}</h3>
+                  ))}
+                  <button>Buy</button>
+                </div>
+              </div>
+            </div>)
+            
+            )}
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
