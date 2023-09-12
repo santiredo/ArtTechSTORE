cfac82ef307c4ffba861bdd391f6d172ee0eb2ff
santiRedondo <santi16redo@gmail.com>
2023-09-12 20:26:02 -0300

    Ajustando el detalle de detail

--- client/src/views/Detail/Detail.tsx
+++ client/src/views/Detail/Detail.tsx
@@ -5,18 +5,12 @@ import { InitialState } from '../../redux/reducer';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-
+import { getProductById } from "../../redux/action";
 import emptyFav from '../../assets/favEmpty.png'
 import filledFav from '../../assets/favFilled.png'
-import Viaje from "../../images/Viaje.jpg";
+import loadingGif from '../../assets/loading.gif'
+import user from '../../assets/usuario.png'
 import style from "./Detail.module.css";
-
-import { getProductById } from "../../redux/action";
-
-import { useSelector } from 'react-redux';
-import { InitialState } from '../../redux/reducer';
-import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
 
 
 
@@ -27,6 +21,8 @@ const Detail = () => {
     const product = useSelector((state: InitialState) => state.productDetail)
 
     const [isFav, setIsFav] = useState(false)
+
+    const [loading, setLoading] = useState(true)
 
     const handleFav = () => {
         setIsFav(!isFav)
@@ -40,54 +36,66 @@ const Detail = () => {
     useEffect(() => {
         getProductById(id, dispatch)
     
+        product && setLoading(false)
   }, []);
 
 
 
     
     return(
-        <div className={style.detailPage}>
-            <div className={style.postContainer}>
-                <img className={style.postImage} src={Viaje} alt="cuadro" />
-                <div className={style.info}>
-                    <h1>{product.title}</h1>
-                    <h2>{product.price}</h2>
-                    {
-                        isFav ? (
-                            <div className={style.imgFav}>
-                                <img onClick={handleFav} src={filledFav} alt="" />
-                                <span>Remove from favs</span>
-                            </div>
-                            
-                        ) : (
-                            <div className={style.imgFav}>
-                                <img onClick={handleFav} src={emptyFav} alt="" />
-                                <span>Add to favs</span>
-                            </div>
-                        )
-                    }
-                    <p>
-                        <NavLink to={`http://localhost:3001/artist/${product.artistName}`} 
-                        className={style.ownerLink}>{product.artistName}
-                        </NavLink>
-                    </p>
+        <>
+        {
+            loading ? (
+                <div className={style.loadingDiv}>
+                    <img src={loadingGif} alt="" />
+                </div>
 
-                    <h3>Type: Painting</h3>
-                    <h3>Technique: </h3>
-                    <button className={style.buyPostButton}>Add to cart</button>
+            ) : (
+                <div className={style.detailPage}>
+                    <div className={style.postContainer}>
+                        <img className={style.postImage} src={product.image} alt="cuadro" />
+                        <div className={style.info}>
+                            {
+                                isFav ? (
+                                    <div className={style.imgFav}>
+                                        <img onClick={handleFav} src={filledFav} alt="" />
+                                        <span>Remove from favs</span>
+                                    </div>
+                                    
+                                ) : (
+                                    <div className={style.imgFav}>
+                                        <img onClick={handleFav} src={emptyFav} alt="" />
+                                        <span>Add to favs</span>
+                                    </div>
+                                )
+                            }
+                            <h2>{product.title}</h2>
+                            <h3>{product.price}</h3>
+                            <NavLink to={`http://localhost:3001/artist/${product.artistName}`}>
+                            <div className={style.imgDiv}>
+                                <img src={product.artistPhoto ? product.artistPhoto : user} alt="" />
+                            </div>
+                            {product.artistName}
+                            </NavLink>
+                            <h3>{product.type}</h3>
+                            <h3>{product.technique}</h3>
+                            <button className={style.buyPostButton}>Buy now</button>
+                        </div>
+                    </div>
+                    <div className={style.descriptionDiv}>
+                        <div>
+                            <h2>Description</h2>
+                            <h4>{product.description}</h4>
+                        </div>
+                        <hr />
+                        <div>
+                            
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className={style.descriptionDiv}>
-                <div>
-                    <h2>Description</h2>
-                    <h4>text...</h4>
-                </div>
-                <hr />
-                <div>
-                    
-                </div>
-            </div>
-        </div>
+            )
+        }
+        </>
     );
 };
 
