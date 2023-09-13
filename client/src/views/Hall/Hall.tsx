a078d3d6e56a5f12cff30cdffaa8cae0254cc230
santiRedondo <santi16redo@gmail.com>
2023-09-13 15:00:46 -0300

    Agregando hall de entrada para el registro

---
+++ client/src/views/Hall/Hall.tsx
@@ -0,0 +1,57 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { useEffect, useState } from 'react'
+import Register from '../../components/Register/Register'
+import axios from 'axios'
+import loadingGIF from '../../assets/loading.gif'
+import { useNavigate } from 'react-router-dom'
+
+import style from './hall.module.css'
+
+export default function Hall(){
+
+    const [loading, setLoading] = useState(true)
+
+    const navigate = useNavigate()
+
+    const {user} = useAuth0()
+
+    const [userExistence, setUserExistence] = useState(Boolean)
+  
+    const isUserRegistered = async() => {
+      if(user?.email){
+        const userResponse = await axios(`http://localhost:3001/user/mail?mail=${user!.email}`)
+  
+        const artistResponse = await axios(`http://localhost:3001/artist/artist/mail?mail=${user!.email}`)
+  
+        if(!userResponse.data && !artistResponse.data){
+            setLoading(false)
+            setUserExistence(false)
+        } else{
+            navigate('/home')
+        }
+      }
+    }
+
+    useEffect(() => {
+        isUserRegistered()
+    }, [user?.email])
+
+    return (
+        <div className={style.hallPage}>
+            {
+                loading ? (
+                    <>
+                        <img className={style.loading} src={loadingGIF} alt="" />
+                    </>
+                ) : (
+                    <div>
+                        {
+                            !userExistence && <Register onRegister={(userExistence) => setUserExistence(userExistence)} />
+                        }
+                    </div>
+                )
+            }
+        </div>
+    )
+
+}
