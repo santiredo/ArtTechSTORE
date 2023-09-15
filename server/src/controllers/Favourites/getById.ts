87dd27a14fc9a6fc77fb0e38e6195af450c1be01
santiRedondo <santi16redo@gmail.com>
2023-09-15 18:14:14 -0300

    trayendo a los productos con el detalle completo

--- server/src/controllers/Favourites/getById.ts
+++ server/src/controllers/Favourites/getById.ts
@@ -1,11 +1,15 @@
-import { Favourite } from "../../db";
+import { Favourite, Product } from "../../db";
 
 export async function getFavouritesById(UserId: number) {
 
-    const favourites = await Favourite.findAll({
+    const userFavourites = await Favourite.findAll({
         where: {
             UserId
         }
+    })
+
+    const favourites = userFavourites.map(async(fav) => {
+        return await Product.findByPk(fav.dataValues.ProductId)
     })
 
     return favourites
