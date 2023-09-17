351776c9a4c71f15a4b9b7eb8c1c176e6089a310
santiRedondo <santi16redo@gmail.com>
2023-09-17 17:34:12 -0300

    Creo que ya si finalizada la configuracion de la db

--- server/src/db.ts
+++ server/src/db.ts
@@ -6,17 +6,40 @@ import path from 'path'
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DATABASE_URL } = process.env;
 
-
-let sequelize: Sequelize =  new Sequelize(DATABASE_URL!, {
+    const sequelize = new Sequelize({
+    dialect: 'postgres',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: Number(DB_PORT), 
+    database: DB_NAME,
     logging: false,
     native: false,
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+        ssl: {
+            require: true, // Requiere SSL
+            rejectUnauthorized: false // Permite conexiones sin certificados válidos (no recomendado en producción)
+        }
     }
-});
+    })
+
+
+/* const sequelize = new Sequelize({
+    dialect: 'postgres',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: Number(DB_PORT), 
+    database: DB_NAME,
+    logging: false,
+    native: false,
+    dialectOptions: {
+        ssl: {
+            require: true, // Requiere SSL
+            rejectUnauthorized: false // Permite conexiones sin certificados válidos (no recomendado en producción)
+        }
+    }
+}); */
 
 const basename = path.basename(__filename)
 
