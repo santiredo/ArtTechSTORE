df336d1b72a220c52d9b6b5ab6bbe288fd41f673
santiRedondo <santi16redo@gmail.com>
2023-09-18 17:42:30 -0300

    Configurando vite para el deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Agrega una regla para archivos HTML
+      output: {
+        manualChunks: {
+          'index.html': ['index.html'],
+        },
+      },
+    },
+  },
 });
