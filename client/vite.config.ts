51e9e0c1e34607698db869bd271ca5959bb563f9
santiRedondo <santi16redo@gmail.com>
2023-09-19 14:06:47 -0300

    Agregando la url a la configuracion de vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'https://arttechstore-production-37f5.up.railway.app'
+  },
   build: {
     outDir: 'dist', // Carpeta de salida para los archivos generados
   },
