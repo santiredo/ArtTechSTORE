a51d9433c442933beba6ba637b00924fb930f8a5
santiRedondo <santi16redo@gmail.com>
2023-09-19 17:57:37 -0300

    PORT

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import { sequelize } from "./db";
 import server from "./server";
+import 'dotenv/config';
 
-const { DB_PORT } = process.env;
+const PORT = process.env.PORT || 3001
 
 sequelize.sync({ force: false }).then(() => {
-    server.listen(() => {
-      console.log(`%s listening`);
+    server.listen(PORT, () => {
+      console.log(`Server listening at ${PORT}`);
     });
   })
   .catch((error: any) => {
