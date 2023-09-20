4634c1b50f18fcae1cae829b9872e714ed57e2d1
santiRedondo <santi16redo@gmail.com>
2023-09-20 18:09:56 -0300

    Boton del carrusel

--- client/src/components/Carrusel/Carrusel.tsx
+++ client/src/components/Carrusel/Carrusel.tsx
@@ -1,50 +1,54 @@
 import { Carousel } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
-import { ArtGalleryItem } from '../../redux/reducer';
+import { useEffect } from 'react';
+import { InitialState } from '../../redux/reducer';
 import user from '../../assets/usuario.png'
 import style from './carrusel.module.css'
+import { NavLink } from 'react-router-dom';
+import { getAllProducts } from '../../redux/action';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 
 
-export default function Carrusel(){
+export default function Carrusel() {
 
-    const [items, setItems] = useState<ArtGalleryItem[]>([]);
+  const products = useSelector((state: InitialState) => state.allPosts)
 
-    console.log(items)
+  const dispatch = useDispatch()
 
-    const URL='http://localhost:3001'
-    useEffect(() => {
-        fetch(`${URL}/products`).then(response => response.json()).then(data => setItems(data));
-        
-      }, []);
+  useEffect(() => {
+
+    getAllProducts(dispatch)
+
+  }, []);
 
 
-    return (
-        <div className={style.subcontainer}>
-          <h1>Latest posts</h1>
-          <Carousel className={style.carouselComponent} interval={null}>
-            {
-              items.slice(-3).map((card) => (
-                <Carousel.Item key={card.id}>
-                  <div className={style.cardComponent}>
-                    <div className={style.cardInfo}>
-                        <div className={style.artistData}>
-                            <div className={style.imgDiv}>
-                                <img src={card.artistPhoto ? card.artistPhoto : user} alt="" />
-                            </div>
-                            <h3>{card.artistName}</h3>
-                        </div>
-                        <h3>${card.price}</h3>
-                        <button>Buy</button>
+  return (
+    <div className={style.subcontainer}>
+      <h1>Latest posts</h1>
+      <Carousel className={style.carouselComponent} interval={null}>
+        {
+          products.slice(-3).map((card) => (
+            <Carousel.Item key={card.id}>
+              <div className={style.cardComponent}>
+                <div className={style.cardInfo}>
+                  <div className={style.artistData}>
+                    <div className={style.imgDiv}>
+                      <img src={card.artistPhoto ? card.artistPhoto : user} alt="" />
                     </div>
-                    <div className={style.divImage}>
-                      <img className={style.postImg} src={card.image} alt="" />
-                    </div>
+                    <h3>{card.artistName}</h3>
                   </div>
-                </Carousel.Item>
-              ))
-            }
-          </Carousel>
-      </div>
-    )
+                  <h3>${card.price}</h3>
+                  <NavLink className={style.button}to={`/detail/${card.id}`}>Buy</NavLink>
+                </div>
+                <div className={style.divImage}>
+                  <img className={style.postImg} src={card.image} alt="" />
+                </div>
+              </div>
+            </Carousel.Item>
+          ))
+        }
+      </Carousel>
+    </div>
+  )
 }
