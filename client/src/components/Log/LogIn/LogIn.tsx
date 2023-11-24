71e1919b53341b36d90a0ce04e94f94e1269da31
santiRedondo <santi16redo@gmail.com>
2023-11-24 17:43:43 -0300

    Faltan los estilos del boton de inicio de sesion con google

--- client/src/components/Log/LogIn/LogIn.tsx
+++ client/src/components/Log/LogIn/LogIn.tsx
@@ -1,6 +1,8 @@
 
+import { signInWithPopup } from "firebase/auth";
+import { auth,  provider } from '../googleAuth'
 
-
+import googleIcon from '../../../assets/googleIcon.png'
 import style from './login.module.css'
 
 interface SignUpProps {
@@ -9,6 +11,27 @@ interface SignUpProps {
 
 
 export default function LogIn({onClose}: SignUpProps) {
+
+    // MANEJAMOS EL LOGIN DE GOOGLE
+    const googleLogin = async() => {
+
+        try {
+            const user = await signInWithPopup(auth, provider)
+
+            // Aca usamos la data del usuario logeado para guardarla en el storage
+
+        } catch (error) {
+            alert('Some error has occured')
+            console.log(error)
+        }
+
+    }
+
+    //MANEJAMOS EL LOGIN MANUAL
+
+    const handleLogin = () => {
+        // ACA LLAMAMOS A LA BASE DE DATOS A VERIFICAR SI EL USUARIO ESTA CREADO
+    }
 
     return (
         <div className={style.backgroundShadow}>
@@ -20,8 +43,14 @@ export default function LogIn({onClose}: SignUpProps) {
                 <br />
                 <input type="text" name="" id="" placeholder='Password'/>
                 <br />
-                <button>Enter</button>
+                <button onClick={handleLogin}>Enter</button>
                 <br />
+                <p>- Or -</p>
+                <br />
+                <button className={style.googleAuthButton} onClick={googleLogin}>
+                    <img src={googleIcon} alt="" />
+                    Log in with Google
+                </button>
             </form>
         </div>
         </div>
