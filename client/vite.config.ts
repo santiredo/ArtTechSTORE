008c7b76d876b50d2beb8ffa4bd60c0c676645e1
santiRedondo <santi16redo@gmail.com>
2023-11-25 19:07:19 -0300

    Modificando la configuracion de vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Carpeta de salida para los archivos generados
-  }
-})
+    outDir: 'dist',
+    minify: 'esbuild',
+    assetsInlineLimit: 0, // También puedes intentar deshabilitar el límite de tamaño para incrustar archivos
+    chunkSizeWarningLimit: 2000, // Puedes ajustar este límite según tus necesidades
+    manifest: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
