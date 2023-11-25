ea65694544bc7733d7fa4170bb5930dd4e4ea5b8
santiRedondo <santi16redo@gmail.com>
2023-11-25 17:10:14 -0300

    Actualizando el main.tsx, sacando configuracion de auth0

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux'
 import './index.css'
 import { store } from './redux/store.ts'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Auth0Provider } from '@auth0/auth0-react';
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3001/'
@@ -15,15 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Auth0Provider
-          domain="dev-bmewhnl6c5tr886s.us.auth0.com"
-          clientId="2sXnU51ibdteMmbhOjNb42oTthkRMkkg"
-          authorizationParams={{
-            redirect_uri: 'http://127.0.0.1:5173/hall'
-          }}
-        >
           <App />
-        </Auth0Provider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
